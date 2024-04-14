e764798beed5f34e0d1c9e812e669e43e3df9d0a
Lena1292 <elena.krylataya@gmail.com>
2024-04-14 20:57:39 +0300

    create homework

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -1,7 +1,11 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class HomeWork {
     WebDriver wd;
@@ -17,8 +21,16 @@ public class HomeWork {
 
 @Test
     public void login(){
+    WebElement a= wd.findElement(By.tagName("a"));
+    List<WebElement> list = wd.findElements(By.tagName("a"));
 
+    WebElement input = wd.findElement(By.tagName("input"));
+    WebElement input1 = wd.findElement(By.tagName("input"));
 
+    WebElement button = wd.findElement(By.tagName("button"));
+    WebElement button1 = wd.findElement(By.tagName("button"));
+
+    List<WebElement> list1 = wd.findElements(By.tagName("div"));
 }
 
 
